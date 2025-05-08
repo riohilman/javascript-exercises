da54e3aad89e26cb056429db7296156d7611886a
Rio Hilman <53969666+riohilman@users.noreply.github.com>
2025-05-08 09:04:08 +0700

    Add function to reverse string using loop

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const strArray = string.split("");
+    let text = "";
+    for (let i = strArray.length - 1; i >= 0; i--) {
+        text += strArray[i];
+    }
+    return text;
 };
 
 // Do not edit below this line
