c3db606f659abed4f513db609e60f9689f6fa3fc
Rio Hilman <53969666+riohilman@users.noreply.github.com>
2025-05-08 10:17:26 +0700

    Update sumAll to handle cases where start is greater than end

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,9 @@
 const sumAll = function(start, end) {
+    if (end < start) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
     let sum = 0;
     for (let i = start; i <= end; i++) {
         sum += i;
