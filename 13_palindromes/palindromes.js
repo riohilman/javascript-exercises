1ca40489b7a5c517a42f1756a4150a86dd5fee0c
Rio Hilman <53969666+riohilman@users.noreply.github.com>
2025-05-18 11:26:53 +0700

    Add function to test palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let regex = /[a-z0-9]/;
+    const string = word
+        .toLowerCase()
+        .split('')
+        .filter((char) => regex.test(char))
+        .join('');
+    const reverseString = string.split('').reverse().join('');
+    return string === reverseString;
 };
 
 // Do not edit below this line
