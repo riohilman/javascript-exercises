1463ab90f9be9fcc61dd38db027831dcbead1757
Rio Hilman <53969666+riohilman@users.noreply.github.com>
2025-05-19 08:08:19 +0700

    Add function to find the oldest using reduce

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    const oldest = people.reduce((oldestPerson, currentPerson) => {
+        const currentAge = (currentPerson.yearOfDeath ?? currentYear) - currentPerson.yearOfBirth;
+        const oldestAge = (oldestPerson.yearOfDeath ?? currentYear) - oldestPerson.yearOfBirth;
 
+        return currentAge > oldestAge ? currentPerson : oldestPerson;
+    })
+
+    return oldest;
 };
 
 // Do not edit below this line
