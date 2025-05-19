9b850e732d44a375e3d66557bc0816eb89bcdec9
Rio Hilman <53969666+riohilman@users.noreply.github.com>
2025-05-19 07:17:48 +0700

    Add function fibonacci using loop

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
 
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+    if (num < 0) return "OOPS";
+    
+    let num1 = 0;
+    let num2 = 1;
+    let fib;
+    for (let i = 2; i <= num; i++) {
+        fib = num1 + num2;
+        num1 = num2;
+        num2 = fib;
+    }
+    return fib;
 };
 
 // Do not edit below this line
